9cfa1467ba8d530dba9ea6d28a561afc45b26248
Jonas Benz <jonas-benz@gmx.de>
2025-04-19 09:17:36 +0200

    add function

---
+++ src/api/Rsvp.cs
@@ -0,0 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api
+{
+    public class Rsvp(ILogger<Rsvp> logger)
+    {
+        private readonly ILogger<Rsvp> _logger = logger;
+
+        [Function("Rsvp")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
